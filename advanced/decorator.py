9601cf7e8d8342b17f644f9dc8cfae2aa0e600c4
rejector7 <2643719428@qq.com>
2020-12-28 13:03:10 +0800

    Initial commit

---
+++ advanced/decorator.py
@@ -0,0 +1,21 @@
+import time
+
+
+def run_time(func):
+    def normal_add_run_time(name):
+        time1 = time.time()
+        func(name)
+        time2 = time.time()
+        t = time2 - time1
+        return print("My name is {0}, run for {1} seconds".format(name, t))
+    return normal_add_run_time
+
+
+@run_time
+def normal_func(name):
+    time.sleep(5)
+    # print("My name is {0}.".format(name))
+
+
+normal_func("rjt")
+
